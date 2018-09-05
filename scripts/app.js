4bac3d59954e70d219080f88283223dcb378ca9f
Andreas Norberg <fiskkneipp@gmial.com>
2018-09-05 15:57:45 +0200

    klar i skolan 5/9

--- scripts/app.js
+++ scripts/app.js
@@ -3,11 +3,22 @@ import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 
 $(function () {
     $("#searchBtn").click(function () {
-        $.get('/api/search/' + $('#searchString').val(), function (data) {
-            alert(data.StopLocation);
+        $.get('/api/search/' + $('#searchString').val(), function (data) {              
             $.each(data.StopLocation, function (index, stops, id) {
                 $('#outputUl').append('<li class="liid" id=' + stops.id + '>' + 'hållplats: ' + stops.name + '</li>');
-            })
+                $("li").hover(function(){
+                    $(this).css("background-color", "gray");
+                    }, function(){
+                    $(this).css("background-color", "white");
+                });                
+            });            
         });
     });
+
+    $(".liid").hover(function () {
+        $(this).toggleClass('hover');
+        alert('hover');
+    });
+
+    
 });
